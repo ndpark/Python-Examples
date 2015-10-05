4141f3210fbfedfbcac39901647afbb2755b0017
Andy Park <hg4park@uwaterloo.ca>
2015-10-05 19:13:03 -0400

    Revert "Examples"
    This reverts commit 4018731918c641df345760cd6cc9fd008c4ac614.

--- ex1.py
+++
@@ -1,9 +0,0 @@
-print "Hello World!"
-print "Hello Again."
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rther you 'not'."
-print 'I "said" do not touch this.'
-print "Do this"
-print "This is exercise one"
