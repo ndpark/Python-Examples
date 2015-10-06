cd7a854490549db9a8523c5620976360cc2b4d60
Andy Park <hg4park@uwaterloo.ca>
2015-10-06 01:05:22 -0400

    Python examples
    Examples of coding using book "Learn Python the Hard Way"

---
+++ Python/ex2.py
@@ -0,0 +1,9 @@
+#This is a commment
+#Basically anything after # is ignored by python
+
+print "I could have a code like this." #This would be ignored
+
+#Use a comment to disable or comment out a piece of code
+#print "This won't run"
+
+print "This will run"
