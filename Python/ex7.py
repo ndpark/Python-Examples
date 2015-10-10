71d5cb1beb6ad1e2e12355b130f0d58629c5fe02
Andy Park <hg4park@uwaterloo.ca>
2015-10-10 00:32:30 -0400

    Revert "October 1o"
    This reverts commit 2aa6cefc1a16c1e2dda69696bc736ba148321608.

--- Python/ex7.py
+++ Python/ex7.py
@@ -3,7 +3,7 @@ print "Its fleece was as white as %s." % 'snow'
 print "And everywhere that Mary went."
 print "." * 10 #This multipies the string by n
 
-end1 = "C" #This sets end1 to C... continues to do so till end 12
+end1 = "C"
 end2 = "h"
 end3 = "e"
 end4 = "e"
@@ -19,5 +19,3 @@ end12 = "r"
 
 print end1 + end2 + end3 + end4 + end5+ end6
 print end7 + end8 + end9 + end10 + end11 + end12
-
-#The comma between print and print doesn't matter
