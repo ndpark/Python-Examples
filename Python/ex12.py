223b4c835b8531a775cec152a13f0a2dba8aaf34
Andy Park <hg4park@uwaterloo.ca>
2015-10-11 13:39:50 -0400

    Prompting People
    Using raw_input() more effectively

---
+++ Python/ex12.py
@@ -0,0 +1,12 @@
+#For raw_input(), you can put a string you want prompted inside 
+age = raw_input("How old are you ? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So you are %r old, %r tall, and %r heavy." % (age, height, weight)
+
+#WOW WTF THIS EXPLAINS LIKE PROMPTS FOR ME HOLY MOLY THIS IS SO COOL
+# WRITE A PROMPT AND THE PUT THE CURSOR THING INSIDE THE ()
+
+# python -m pydoc (whatever u wanna see) to findout about the command itself
+# Basically what I discovered like right above lmao
