8238948525b064948a3ce8b2ad18b769642c4028
Andy Park <hg4park@uwaterloo.ca>
2015-10-11 13:38:44 -0400

    What Was That?
    Lesson on escape sequences

--- Python/ex10.py
+++ Python/ex10.py
@@ -10,7 +10,6 @@ tabby_cat = "\tI'm tabbing in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-
 fat_cat = """
 I'll do a list:
 \t* Cat food
@@ -25,6 +24,8 @@ I'll do a list:
 \t* Fishes
 \t* Catnip\n\t* Grass
 '''
+#Not really that much difference between triple ' and triple ".
+
 print tabby_cat
 print persian_cat
 print backslash_cat
@@ -34,3 +35,17 @@ print "This is an example from the text about inside and outside double/single q
 print "\nI am 6'2\" tall" #Basically it's saying the quotation mark is quotation mark, not a operator
 print '\nI am 6\'2" tall'
 #Look at page 39 for all the escape sequences
+
+format = "%s %s %s" 
+print format % ("\n\\ These are inside slashes \\", "\t This is tabbed.", "Hello world \"SWAG\" SWAG should be inside double quotes. \n" )
+print format % ("Let us", "\| Use %r\|", "Instead\n" )
+
+format = "%r %r %r" 
+print format % ("\n\\ These are inside slashes \\", "\t This is tabbed.", "Hello world \"SWAG\" SWAG should be inside double quotes. \n" )
+#Notice how using %r will output literal input rather than translated statement
+
+
+
+
+
+
